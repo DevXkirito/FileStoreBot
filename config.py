8895e1b1529fa88e406200418bcbd9790fae4294
DevXkirito <111516421+DevXkirito@users.noreply.github.com>
2024-09-16 19:37:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 import os
 
-API_ID = int(os.environ.get("API_ID", 0))
-API_HASH = os.environ.get("API_HASH", None)
-BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
-DB_CHANNEL_ID = os.environ.get("DB_CHANNEL_ID")
+API_ID = int(os.environ.get("7391573", 0))
+API_HASH = os.environ.get("1f20df54dfd91bcee05278d3b01da2c7", None)
+BOT_TOKEN = os.environ.get("5285088021:AAGbxRFtj53in80VZXD4WmT2Ds0amMMqB8o", None)
+DB_CHANNEL_ID = os.environ.get("-1002199830633")
 IS_PRIVATE = os.environ.get("IS_PRIVATE",False) # any input is ok But True preferable
-OWNER_ID = int(os.environ.get("OWNER_ID"))
-UPDATE_CHANNEL = os.environ.get('UPDATE_CHANNEL', '')
+OWNER_ID = int(os.environ.get("2092233990"))
+UPDATE_CHANNEL = os.environ.get('UPDATE_CHANNEL', '-1001509463900')
 AUTH_USERS = list(int(i) for i in os.environ.get("AUTH_USERS", "").split(" ")) if os.environ.get("AUTH_USERS") else []
 if OWNER_ID not in AUTH_USERS:
     AUTH_USERS.append(OWNER_ID)
